1086982fb958040f23e6f987d6b53f10a42053d7
roberto.rdz <robertorodriguez@MacBook-Pro-de-Roberto.local>
2020-11-20 11:56:32 -0600

    Creacion de una  carpeta de rutas

--- server/server.js
+++ server/server.js
@@ -9,58 +9,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json
 app.use(bodyParser.json())
- 
+
 app.get('/', function (req, res) {
   res.send('<h1>Bienvenido a mi servidor REST localhost</h1>')
 });
 
-app.get('/usuario', function (req, res) {
-  res.json({
-    ok: '200',
-    mensaje: 'Usuarios consultados correctamente'
-  });});
-
-app.post('/usuario', function (req, res) {
-  let nombre = req.body.nombre;
-  let body = req.body;
-
-  if(nombre === undefined){
-    res.status(400).json({
-      ok: 400,
-      mensaje: 'Favor de enviar el valor del nombre'
-    })
-  }else{
-
-  res.json({
-    ok: '200',
-    mensaje: 'Usuario insertado con exito',
-    body: body
-
-  });
-}
-});
-
-app.put('/usuario/:id/:nombre', function (req, res) {
-  let id = req.params.id;
-  let nombre = req.params.nombre;
-
-  res.json({
-    ok: '200',
-    mensaje: 'Usuario actualizado con exito',
-    id: id,
-    nombre: nombre
-  });
-});
-
-app.delete('/usuario/:id', function(req, res){
-  let id = req.params.id;
-
-  res.json({
-    ok: '200',
-    mensaje: 'Usuario eliminado con exito',
-    id: id
-  });
-});
+app.use(require('./routes/usuario'));
 
 mongoose.connect('mongodb://localhost:27017/cafeteria', {
   useNewUrlParser: true,
