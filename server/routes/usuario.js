bc721c3fefcf9f0860b61c466a935c5801dfe5b2
roberto.rdz <robertorodriguez@MacBook-Pro-de-Roberto.local>
2020-12-01 13:22:23 -0600

    Realizado el GET y PUT de la ruta usuario

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const _ = require('underscore');
 const Usuario = require('../models/usuario');
 const app = express();
   
   app.get('/usuario', function (req, res) {
-    res.json({
-      ok: '200',
-      mensaje: 'Usuarios consultados correctamente'
+    let desde = req.query.desde || 0;
+    let hasta = req.query.hasta || 5;
+    Usuario.find({estado: true})
+    .skip(Number(desde))
+    .limit(Number(hasta))
+    .exec((err, usuarios) => {
+      if(err){
+        return res.status(400).json({
+          ok: false,
+          msg: 'Ocurrio un error al momento de consultar',
+          err
+        })
+      }
+
+      res.json({
+        ok: true,
+        msg: 'Lista de usuarios obtenida con exito',
+        conteo: usuarios.length,
+        usuarios
+      })
     });
   });
   
@@ -34,16 +52,25 @@ const app = express();
     });
   });
   
-  app.put('/usuario/:id/:nombre', function (req, res) {
-    let id = req.params.id;
-    let nombre = req.params.nombre;
-  
+  app.put('/usuario/:id', function (req, res) {
+   let id = req.params.id;
+   let body = _.pick(req.body, ['nombre', 'email']);
+
+   Usuario.findByIdAndUpdate(id, body, {new: true, runValidators: true, context: 'query'}, 
+   (err, usrDB) => {
+    if(err){
+      return res.status(400).json({
+        ok: false,
+        msg: 'Ocurrio un error al momento de actualizar',
+        err
+      });
+    }
     res.json({
-      ok: '200',
-      mensaje: 'Usuario actualizado con exito',
-      id: id,
-      nombre: nombre
+      ok: true,
+      msg: 'Usuario actualizado con exito',
+      usuario: usrDB
     });
+   });
   });
   
   app.delete('/usuario/:id', function(req, res){
